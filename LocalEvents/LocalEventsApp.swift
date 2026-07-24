5f297881b70bbb4a9fbdb4ce2dc7b50401a8b2c5
Shahab <s.shahabhmad@gmail.com>
2026-07-24 05:32:10 -0400

    First commit

---
+++ LocalEvents/LocalEventsApp.swift
@@ -0,0 +1,19 @@
+//
+//  LocalEventsApp.swift
+//  LocalEvents
+//
+//  Created by Shahab Ahmad on 24/07/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct LocalEventsApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
---
+++ LocalEventsTests/LocalEventsTests.swift
@@ -0,0 +1,19 @@
+//
+//  LocalEventsTests.swift
+//  LocalEventsTests
+//
+//  Created by Shahab Ahmad on 24/07/26.
+//
+
+import Testing
+@testable import LocalEvents
+
+struct LocalEventsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
